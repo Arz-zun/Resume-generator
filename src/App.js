636c30638fef0b1885b48106d55136925c60eb89
Arz-zun <arjunchaudhary757@gmail.com>
2023-01-27 19:43:37 +0545

    redux setup and data fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import Home from './components/Home';
+import { useEffect } from "react";
+import { fetchalluser } from "./api/services/AllUser";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchalluser());
+  }, []);
   return (
 <div className=''>
   <Home/>
