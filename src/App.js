da2791988078c35817fd237e2b6547379242bdea
Arz-zun <arjunchaudhary757@gmail.com>
2023-01-27 20:00:28 +0545

    user details is displayed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./components/Home";
+import User from "./components/User";
 import { useEffect } from "react";
 import { fetchalluser } from "./api/services/AllUser";
 import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const dispatch = useDispatch();
@@ -11,9 +13,12 @@ function App() {
     dispatch(fetchalluser());
   }, []);
   return (
-<div className=''>
-  <Home/>
-</div>
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user/:userid" element={<User />} />
+      </Routes>
+    </div>
   );
 }
 
